76ec14439375ab7404425e2228f8200010beb8b8
Ashley Callahan <info@ashleycallahan.net>
2013-12-30 16:31:12 -0500

    Update likes count to store to cache

--- _php/facebook/feed.php
+++ _php/facebook/feed.php
@@ -12,7 +12,7 @@
 	$cachetime = 7200; 
 	
 	// Server path to parent folder
-	$cachepath = "/public_html/custom-php-facebook-feed/_php/facebook/cache.txt";
+	$cachepath = "/hermes/bosoraweb162/b1007/ipw.arc2/public_html/custom-php-facebook-feed/_php/facebook/cache.txt";
 	
 	// CREATE A FACEBOOK APPLICATION TO GET THE FOLLOWING VARIABLES (https://developers.facebook.com/apps)
 	
@@ -40,20 +40,31 @@
 		$jsonurl = "https://graph.facebook.com/".$facebookuser."/posts?limit=".$noposts."&".$access_token;
 		$json = file_get_contents($jsonurl,0,null,null);
 		$json_output = json_decode($json, true);
+		for($i=0; $i<$noposts; $i++){
+			$jsonurl_likes = "https://graph.facebook.com/".$json_output[data][$i][object_id]."/likes?summary=1";
+			$json_likes = file_get_contents($jsonurl_likes,0,null,null);
+			if($i==0){
+				$likes .= $json_likes;
+			} else {
+				$likes .= ", ".$json_likes;
+			}
+		}
+		$json_combined = '{"posts":['.$json.'], "likes":['.$likes.']}';
+		$json_output = json_decode($json_combined, true);
 		$fp = fopen($cachepath, 'w');
-		fwrite($fp, $json);
+		fwrite($fp, $json_combined);
 		fclose($fp);
 	}
 	$posts = $json_output;
 	echo "<ul class='facebook-custom-feed'>";
 	for($i=0; $i<$noposts; $i++){
 		echo "<li><p>";
-		echo "<a href='https://www.facebook.com/".$facebookuser."' target='_blank' class='facebook-custom-feed-account'>".$posts['data'][$i][from][name]."</a><br />";
+		echo "<a href='https://www.facebook.com/".$facebookuser."' target='_blank' class='facebook-custom-feed-account'>".$posts[posts][0][data][$i][from][name]."</a><br />";
 		date_default_timezone_set($timezone);
-		echo "<span class='facebook-custom-feed-date'>".date($dateformat,strtotime($posts['data'][$i][created_time]))."</span><br />";
-		if($posts['data'][$i][message]){
+		echo "<span class='facebook-custom-feed-date'>".date($dateformat,strtotime($posts[posts][0][data][$i][created_time]))."</span><br />";
+		if($posts[posts][0][data][$i][message]){
 			$post_message = "";
-			$post_message = $posts['data'][$i][message];
+			$post_message = $posts[posts][0][data][$i][message];
 			$post_message_words = "";
 			$post_message_words = explode(" ",$post_message);
 			$post_message_word_clean = "";
@@ -65,33 +76,29 @@
 			}
 			echo "<span class='facebook-custom-feed-message'>".$post_message_word_clean."</span></p>";
 		}
-		if($posts['data'][$i][picture]){
+		if($posts[posts][0][data][$i][picture]){
 			echo "<p class='facebook-custom-feed-photo'>";
-			if($posts['data'][$i][link]){
-				echo "<a href='".$posts['data'][$i][link]."' target='_blank'>";
+			if($posts[posts][0][data][$i][link]){
+				echo "<a href='".$posts[posts][0][data][$i][link]."' target='_blank'>";
 			}
-			echo "<img src='".$posts['data'][$i][picture]."' class='facebook-custom-feed-photo'/>";
-			if($posts['data'][$i][link]){
+			echo "<img src='".$posts[posts][0][data][$i][picture]."' class='facebook-custom-feed-photo'/>";
+			if($posts[posts][0][data][$i][link]){
 				echo "</a>";
 			}
-			if($posts['data'][$i][link] && $posts['data'][$i][name]){
-				echo "<a href='".$posts['data'][$i][link]."' target='_blank' class='facebook-custom-feed-photo-link'>".$posts['data'][$i][name]."</a><br />";
+			if($posts[posts][0][data][$i][link] && $posts[posts][0][data][$i][name]){
+				echo "<a href='".$posts[posts][0][data][$i][link]."' target='_blank' class='facebook-custom-feed-photo-link'>".$posts[posts][0][data][$i][name]."</a><br />";
 			}
-			if($posts['data'][$i][caption]){
-				echo "<span class='facebook-custom-feed-photo-caption'>".$posts['data'][$i][caption]."</span><br />";
+			if($posts[posts][0][data][$i][caption]){
+				echo "<span class='facebook-custom-feed-photo-caption'>".$posts[posts][0][data][$i][caption]."</span><br />";
 			}
-			if($posts['data'][$i][description]){
-				echo "<span class='facebook-custom-feed-photo-description'>".$posts['data'][$i][description]."</span>";
+			if($posts[posts][0][data][$i][description]){
+				echo "<span class='facebook-custom-feed-photo-description'>".$posts[posts][0][data][$i][description]."</span>";
 			}
 			echo "</p>";
 		}
-		$jsonurl_likes = "https://graph.facebook.com/".$posts['data'][$i][object_id]."/likes?summary=1";
-		$json_likes = file_get_contents($jsonurl_likes,0,null,null);
-		$json_output_likes = json_decode($json_likes, true);
-		$likes = $json_output_likes;
-		echo "<p class='facebook-custom-feed-likes'><span class='facebook-custom-feed-likes'>".$likes['summary'][total_count]." people like this</span>";
-		echo " &#149; <a href='".$posts['data'][$i][link]."' target='_blank' class='facebook-custom-feed-like'>Like</a>";
-		echo " &#149; <a href='http://www.facebook.com/sharer/sharer.php?u=".$posts['data'][$i][link]."' target='_blank' class='facebook-custom-feed-share'>Share</a>";
+		echo "<p class='facebook-custom-feed-likes'><span class='facebook-custom-feed-likes'>".$posts[likes][$i][summary][total_count]." people like this</span>";
+		echo " &#149; <a href='".$posts[posts][0][data][$i][link]."' target='_blank' class='facebook-custom-feed-like'>Like</a>";
+		echo " &#149; <a href='http://www.facebook.com/sharer/sharer.php?u=".$posts[posts][0][data][$i][link]."' target='_blank' class='facebook-custom-feed-share'>Share</a>";
 		echo "</p></li>";
 	}
 	echo "</ul>";
